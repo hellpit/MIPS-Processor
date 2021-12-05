ccb540effe1e6a9722d93dc41a7c2146087210c5
Matthew Graca <mgraca@cpp.edu>
2021-12-05 15:01:20 -0800

    fully implemented data memory and tb

--- DataMemory.vhd
+++ DataMemory.vhd
@@ -5,6 +5,7 @@ use ieee.numeric_std.all;
 
 entity DataMemory is
     port(
+		clk : in std_logic;
 		address : in std_logic_vector(31 downto 0);
 		writeData : in std_logic_vector(31 downto 0);
 		memWrite, memRead : in std_logic;
@@ -13,11 +14,37 @@ entity DataMemory is
 end DataMemory;
 
 architecture Behavorial of DataMemory is
-
-begin --begin architecture
-    	process(memWrite, memRead)
+	type ArrayOf32Bits is array(0 to 15) of std_logic_vector(31 downto 0);
+	signal memory : ArrayOf32Bits :=(
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000",
+		x"00000000"
+	);
+-- assumes that the address starts at x00000000
+begin
+    	process(clk, memWrite, memRead)
 	begin
-
+		if(rising_edge(clk)) then
+			if (memWrite = '1') then
+				memory(to_integer(unsigned(address)) / 4) <= writeData;
+			end if;
+			if (memRead = '1') then
+				readData <= memory(to_integer(unsigned(address)) / 4);
+			end if;
+		end if;
 	end process;
 
 end Behavorial;
--- DataMemory_tb.vhd
+++ DataMemory_tb.vhd
@@ -12,6 +12,7 @@ architecture arch of DataMemory_tb is
 	constant T : time := 20 ns;
 
 	--input
+	signal clk : std_logic;
 	signal address : std_logic_vector(31 downto 0);
 	signal writeData : std_logic_vector(31 downto 0);
 	signal memRead, memWrite : std_logic;
@@ -30,14 +31,24 @@ architecture arch of DataMemory_tb is
 	type test_vector_array is array (natural range <>) of test_vector;
 	constant test_vectors : test_vector_array := (
 		--addr, write, memR, memW, read
-		(x"00000000", x"00000000", '0', '0', x"00000000"),
-		(x"00000000", x"00000000", '0', '0', x"00000000")
+		(x"00000000", x"00000000", '1', '0', x"00000000"), --read 0th element
+		(x"00000004", x"aaaaaaaa", '0', '1', x"00000000"), --write xAAAAAAAA to 1st element
+		(x"00000004", x"00000000", '1', '0', x"aaaaaaaa"), --read 1st element (expect xAAAAAAAA)
+		(x"00000004", x"aeaeaeae", '0', '1', x"aaaaaaaa"), --write xAEAEAEAE to 1st element
+		(x"00000008", x"bcbcbcbc", '0', '1', x"aaaaaaaa"), --write xBCBCBCBC to 2nd element
+		(x"0000000C", x"12345678", '0', '1', x"aaaaaaaa"), --write x12345678 to 3rd element
+		(x"0000003C", x"00000000", '1', '0', x"00000000"), --read 15th element (expect x00000000)
+		(x"0000000C", x"00000000", '1', '0', x"12345678"), --read 3rd element (expect x12345678)	
+		(x"00000008", x"00000000", '1', '0', x"BCBCBCBC"), --read 2nd element (expect xBCBCBCBC)	
+		(x"00000004", x"00000000", '1', '0', x"AEAEAEAE"), --read 1st element (expect xAEAEAEAE)
+		(x"00000000", x"00000000", '1', '0', x"00000000") --read 0th element (expect x00000000)				
 		);
 
 begin
 
 	DataMemory_unit : entity work.DataMemory
 		port map(
+				clk=>clk,
 				address=>address,
 				writeData=>writeData,
 				memRead=>memRead,
@@ -48,11 +59,15 @@ begin
     	process 
     	begin
 		for i in test_vectors'range loop
+			clk <= '0';
+			wait for T/2;
+
 			address <= test_vectors(i).address;
 			writeData <= test_vectors(i).writeData;
 			memRead <= test_vectors(i).memRead;
 			memWrite <= test_vectors(i).memWrite;
 
+			clk <= '1';
 			wait for T/2;
 			
 			assert	(
