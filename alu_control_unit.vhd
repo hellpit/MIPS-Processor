8716da9717084323cdcbe785a83cf0c3dfac176a
ricardoaxelbaeza <77639742+ricardoaxelbaeza@users.noreply.github.com>
2021-12-05 20:23:42 -0800

    added immediate instruction functionality

--- alu_control_unit.vhd
+++ alu_control_unit.vhd
@@ -7,7 +7,7 @@ entity ALU_Control_Unit is
     port( clk: in std_logic;
 	  ALU_Function: in std_logic_vector (5 downto 0); -- 6 bits 
 	  ALU_Op: in std_logic_vector (1 downto 0); -- 2bits that will be taken from control unit
-	  Operation: out std_logic_vector (2 downto 0) -- 3 bits that will be passed for ALU to do operation
+	  Operation: out std_logic_vector (3 downto 0) -- 3 bits that will be passed for ALU to do operation
      );
 end ALU_Control_Unit;
 
@@ -19,20 +19,37 @@ architecture behavior_ALU_Control_Unit of ALU_Control_Unit is
 	            if(ALU_OP = "01") then
 		        case ALU_Function is
 		        when "100000" => --add
- 		        Operation <= "010";
+ 		        Operation <= "0010";
 		        when "100010" => --sub
-		        Operation <= "110";
+		        Operation <= "0110";
 		        when "100100" => --and
-		        Operation <= "000";
+		        Operation <= "0000";
 		        when "100101" => --or
-		        Operation <= "001";
+		        Operation <= "0001";
 		        when "101010" => --slt
-		        Operation <= "111";
-		        when others => Operation <= "XXX"; --xor
+		        Operation <= "0111";
+			when "111010" => --this is made up
+			Operation <= "0100"; --xor
+		        when others => Operation <= "XXXX";
 		        end case;
-		    end if;
+		    end if;--r type instr
+		    if (ALU_OP = "10") then 
+		        case ALU_Function is
+			when "001000" => 
+			Operation <= "1010"; --addi
+			when "001100" => 
+			Operation <= "0000"; --andi
+			when "001101" => 
+			Operation <= "1001"; --ori
+			when "001010" =>
+			Operation <= "1111"; -- slti
+			when others => Operation <= "XXXX"; 
+		        end case;
+		    end if; --i type instr
 		end if; --for clk
-		--if(ALU_OP
 	end process;
 end;
+
+
+
 
