ff10d93fe67b88b6054fbcd01904c539ce662c1c
ricardoaxelbaeza <77639742+ricardoaxelbaeza@users.noreply.github.com>
2021-12-06 01:42:22 -0800

    immediate instructions handled for ALU

--- ALU.vhd
+++ ALU.vhd
@@ -6,6 +6,7 @@ entity alu is
 
 	port (
 		input1, input2: in STD_LOGIC_VECTOR (31 downto 0);
+		immediateInput: in STD_LOGIC_VECTOR (15 downto 0); --16 bit 
 		alu_control: in STD_LOGIC_VECTOR (3 downto 0);
 		result: out STD_LOGIC_VECTOR (31 downto 0);
 		zero: out STD_LOGIC
@@ -35,6 +36,16 @@ begin
 					aluResult <= x"00000000";
 				end if;
 			when "1100" => aluResult <= input1 nor input2;
+			when "1010" => aluResult <= std_logic_vector(unsigned(input1) + (unsigned("0000000000000000" & immediateInput))); --addi
+			when "1000" => aluResult <= input1 and "0000000000000000" & immediateInput; --andi
+			when "1001" => aluResult <= input1 or "0000000000000000" & immediateInput; --ori
+			when "1111" =>
+				if (signed(input1) < signed("0000000000000000" & immediateInput)) then
+					aluResult <= x"00000001";
+				else
+					aluResult <= x"00000000";
+				end if;
+
 			when others => null;
 			aluResult <= x"00000000";
 
