2b5bd3eb8fae76b9297b15f34580ef0e7dbbfde4
ricardoaxelbaeza <77639742+ricardoaxelbaeza@users.noreply.github.com>
2021-12-05 18:01:07 -0800

    added clk to alu control unit tb for simulation

--- alu_control_unit_tb.vhd
+++ alu_control_unit_tb.vhd
@@ -9,6 +9,7 @@ architecture arch of alu_control_unit_tb is
     constant T : time := 20 ns;
 
     --input
+    signal clk : std_logic;
     signal ALU_Function: std_logic_vector (5 downto 0); -- 6 bits
     signal ALU_Op : std_logic_vector (1 downto 0); -- 2bits that will be taken from control unit
     --output
@@ -28,13 +29,15 @@ architecture arch of alu_control_unit_tb is
 	("01", "100000", "010"), --add
 	("01", "100010", "110"), --sub
 	("01", "100100", "000"), --and
-	("01", "100101", "001") --or
+	("01", "100101", "001"), --or
+	("01", "101010", "111") --slt
 	--('1', "------", '0', '0', '0', '0', '0', '0', '0', '0', "00")    --dont care
     );
 begin
 
     control_unit_unit : entity work.alu_control_unit--INSERT FILENAME HERE
         port map( 
+		clk => clk,
 		ALU_Op => ALU_Op, 
 		ALU_Function => ALU_Function, 
 		Operation => Operation
@@ -46,13 +49,13 @@ begin
         begin
 
         for i in test_vectors'range loop
-            --clk <= '0';
+            clk <= '0';
             wait for T/2;
 
             ALU_Op <= test_vectors(i).ALU_Op;
             ALU_Function <= test_vectors(i).ALU_Function;
 
-            --clk <= '1';
+            clk <= '1';
             wait for T/2;
             
             assert    (
@@ -66,4 +69,5 @@ begin
         end process;
 
 end arch;
+
 
