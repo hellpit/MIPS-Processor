b0261d0f75094d3163cb1d6bd590f37d506d164e
ricardoaxelbaeza <77639742+ricardoaxelbaeza@users.noreply.github.com>
2021-12-05 22:54:13 -0800

    bet added, & alu op changed to 3 bits

--- alu_control_unit.vhd
+++ alu_control_unit.vhd
@@ -1,84 +1,68 @@
+--@author: Ricardo Baeza, Matthew Graca, Jorge Aranda, Nathan Goshay
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity alu_control_unit_tb is
-end alu_control_unit_tb;
+entity ALU_Control_Unit is
+    port( clk: in std_logic;
+	  ALU_Function: in std_logic_vector (5 downto 0); -- 6 bits 
+	  ALU_Op: in std_logic_vector (2 downto 0); -- 2bits that will be taken from control unit
+	  Operation: out std_logic_vector (3 downto 0) -- 3 bits that will be passed for ALU to do operation
+     );
+end ALU_Control_Unit;
 
-architecture arch of alu_control_unit_tb is
-    constant T : time := 20 ns;
+architecture behavior_ALU_Control_Unit of ALU_Control_Unit is
+    begin --begin architecture
+	process(ALU_Op, ALU_Function)
+	    begin
+		if(rising_edge(clk)) then
+	            if(ALU_OP = "010") then --r-type
+		        case ALU_Function is
+		        when "100000" => --add
+ 		        Operation <= "0010";
+		        when "100010" => --sub
+		        Operation <= "0110";
+		        when "100100" => --and
+		        Operation <= "0000";
+		        when "100101" => --or
+		        Operation <= "0001";
+		        when "101010" => --slt
+		        Operation <= "0111";
+			when "111010" => --this is made up
+			Operation <= "0100"; --xor
+		        when others => Operation <= "XXXX";
+		        end case;
+		    end if;--r type instr
+		    if (ALU_OP = "101") then --i type
+		        case ALU_Function is
+			when "001000" => 
+			Operation <= "1010"; --addi
+			when "001100" => 
+			Operation <= "1000"; --andi
+			when "001101" => 
+			Operation <= "1001"; --ori
+			when "001010" =>
+			Operation <= "1111"; -- slti
+			when others => Operation <= "XXXX"; 
+		        end case;
+		    end if; --i type instr
+		    if (ALU_OP = "001") then -- branch 
+		        case ALU_Function is --6 bit op code taken in 
+			when "000100" =>
+			Operation <= "1100"; --beq (mu)
+			when "000101" =>
+			Operation <= "1101"; --bne (mu)
+			when "000110" =>
+			Operation <= "1110"; --blez (mu)
+			when others => Operation <= "XXXX";
+			end case;
+		    end if; --beq
+		end if; --for clk
+	end process;
+end;
 
-    --input
-    signal clk : std_logic;
-    signal ALU_Function: std_logic_vector (5 downto 0); -- 6 bits
-    signal ALU_Op : std_logic_vector (2 downto 0); -- 3 bits that will be taken from control unit
-    --output
-    signal Operation : std_logic_vector (3 downto 0); -- 4 bits that will be passed for ALU to do operation
 
-    -- declare record type
-    type test_vector is record
-	ALU_Op : std_logic_vector (2 downto 0);
-        ALU_Function : std_logic_vector (5 downto 0);
-        Operation : std_logic_vector (3 downto 0);
-    end record;
 
-    type test_vector_array is array (natural range <>) of test_vector;
-    constant test_vectors : test_vector_array := 
-    (
-       --ALU_Op, ALU Function, Operation
-	--rtype
-	("010", "100000", "0010"), --add
-	("010", "100010", "0110"), --sub
-	("010", "100100", "0000"), --and
-	("010", "100101", "0001"), --or
-	("010", "101010", "0001"), --slt
-	("010", "111010", "0100"), --slt
-	--itype
-	("101", "001000", "1111"), --addi
-	("101", "001100", "0000"), --andi
-	("101", "001101", "1001"),  --ori
-	("101", "001010", "1111"), --slti
-	--branch
-	("001", "000100", "1100"), --beq
-	("001", "000101", "1101"), --bne
-	("001", "000110", "1110")  --blez
-    );
-begin
-
-    control_unit_unit : entity work.alu_control_unit--INSERT FILENAME HERE
-        port map( 
-		clk => clk,
-		ALU_Op => ALU_Op, 
-		ALU_Function => ALU_Function, 
-		Operation => Operation
-                
-                 );
-
-    --cont. clock
-        process 
-        begin
-
-        for i in test_vectors'range loop
-            clk <= '0';
-            wait for T/2;
-
-            ALU_Op <= test_vectors(i).ALU_Op;
-            ALU_Function <= test_vectors(i).ALU_Function;
-
-            clk <= '1';
-            wait for T/2;
-            
-            assert    (
-                    (Operation = test_vectors(i).Operation)
-                )            
-                    -- image is used for string-representation of integer etc.
-                    report  "test_vector " & integer'image(i) & " failed "
-                        severity error;
-            end loop;
-        wait;
-        end process;
-
-end arch;
 
 
 
